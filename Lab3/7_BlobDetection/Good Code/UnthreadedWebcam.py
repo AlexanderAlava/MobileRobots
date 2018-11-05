0d1261cc7af378eab4d36992dab2d762bf5cf535
AlexanderAlava <alava@mail.usf.edu>
2018-11-05 04:11:22 -0500

    UPLOAD

---
+++ Lab3/7_BlobDetection/Good Code/UnthreadedWebcam.py
@@ -0,0 +1,30 @@
+# This is a modified version of the ThreadedWebcam class which removes threading.
+
+import cv2 as cv
+
+class UnthreadedWebcam:
+	def __init__(self, src=0, name="ThreadedWebcam"):
+		self.stream = cv.VideoCapture(src)
+		self.stream.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+		self.stream.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+		#self.stream.set(cv.CAP_PROP_FRAME_WIDTH, 160)
+		#self.stream.set(cv.CAP_PROP_FRAME_HEIGHT, 120)
+		#self.stream.set(cv.CAP_PROP_BUFFERSIZE, 1)
+		
+		if not self.stream.isOpened():
+			print("Failed to open camera!")
+			exit()
+		else:
+			print("Unthreaded webcam started.")
+			
+	def start(self):
+		# The pass statement is a "do nothing" statement.
+		pass
+		
+	def read(self):
+		(self.grabbed, self.frame) = self.stream.read()
+		return self.frame
+	
+	def stop(self):
+		pass
+
