3f3a8c3f6edba2fc3949ed494fbcd4ef5deadd14
AlexanderAlava <alava@mail.usf.edu>
2018-11-14 18:14:57 -0500

    Lab4 following

---
+++ Lab4/following.py
@@ -0,0 +1,266 @@
+import time
+import Adafruit_PCA9685
+import sys
+sys.path.append('/home/pi/VL53L0X_rasp_python/python')
+import VL53L0X
+import RPi.GPIO as GPIO
+import signal
+import math
+
+# Declaring and defining the left and right servos maps constructed with data generated from calibrateSpeeds(
+lPwmTranslation = {
+                    0.00: 1.50, 0.01: 1.505, 0.02: 1.505, 0.03: 1.51, 0.04: 1.51,
+                    0.05: 1.51, 0.06: 1.51, 0.07: 1.51, 0.08: 1.515, 0.09: 1.515,
+                    0.10: 1.515, 0.11: 1.515, 0.12: 1.515, 0.13: 1.52, 0.14: 1.52,
+                    0.15: 1.52, 0.16: 1.52, 0.17: 1.52, 0.18: 1.525, 0.19: 1.525,
+                    0.20: 1.525, 0.21: 1.525, 0.22: 1.525, 0.23: 1.525, 0.24: 1.53,
+                    0.25: 1.53, 0.26: 1.53, 0.27: 1.53, 0.28: 1.53, 0.29: 1.535,
+                    0.30: 1.535, 0.31: 1.535, 0.32: 1.535, 0.33: 1.54, 0.34: 1.54,
+                    0.35: 1.54, 0.36: 1.54, 0.37: 1.54, 0.38: 1.545, 0.39: 1.545,
+                    0.40: 1.545, 0.41: 1.545, 0.42: 1.545, 0.43: 1.55, 0.44: 1.55,
+                    0.45: 1.55, 0.46: 1.55, 0.47: 1.56, 0.48: 1.555, 0.49: 1.555,
+                    0.50: 1.555, 0.51: 1.555, 0.52: 1.555, 0.53: 1.56, 0.54: 1.56,
+                    0.55: 1.56, 0.56: 1.56, 0.57: 1.56, 0.58: 1.565, 0.59: 1.565,
+                    0.60: 1.565, 0.61: 1.565, 0.62: 1.565, 0.63: 1.57, 0.64: 1.57,
+                    0.65: 1.57, 0.66: 1.57, 0.67: 1.57, 0.68: 1.575, 0.69: 1.575,
+                    0.70: 1.58, 0.71: 1.58, 0.72: 1.585, 0.73: 1.585, 0.74: 1.59,
+                    0.75: 1.59, 0.76: 1.59, 0.77: 1.60, 0.78: 1.60, 0.79: 1.61,
+                    0.80: 1.61, 0.81: 1.61, 0.82: 1.61, 0.83: 1.62, 0.84: 1.63,
+                    0.85: 1.64, 0.86: 1.65, 0.87: 1.70
+                    }
+rPwmTranslation = {
+                    0.00: 1.50, 0.01: 1.50, 0.02: 1.50, 0.03: 1.505, 0.04: 1.505,
+                    0.05: 1.505, 0.06: 1.505, 0.07: 1.505, 0.08: 1.51, 0.09: 1.51,
+                    0.10: 1.51, 0.11: 1.51, 0.12: 1.51, 0.13: 1.515, 0.14: 1.515,
+                    0.15: 1.515, 0.16: 1.515, 0.17: 1.515, 0.18: 1.52, 0.19: 1.52,
+                    0.20: 1.52, 0.21: 1.52, 0.22: 1.52, 0.23: 1.525, 0.24: 1.525,
+                    0.25: 1.525, 0.26: 1.525, 0.27: 1.52, 0.28: 1.53, 0.29: 1.53,
+                    0.30: 1.53, 0.31: 1.53, 0.32: 1.53, 0.33: 1.535, 0.34: 1.535,
+                    0.35: 1.535, 0.36: 1.535, 0.37: 1.54, 0.38: 1.54, 0.39: 1.54,
+                    0.40: 1.54, 0.41: 1.54, 0.42: 1.54, 0.43: 1.5425, 0.44: 1.5425,
+                    0.45: 1.5425, 0.46: 1.545, 0.47: 1.545, 0.48: 1.5475, 0.49: 1.5475,
+                    0.50: 1.5475, 0.51: 1.5475, 0.52: 1.55, 0.53: 1.55, 0.54: 1.55,
+                    0.55: 1.55, 0.56: 1.555, 0.57: 1.555, 0.58: 1.555, 0.59: 1.56,
+                    0.60: 1.56, 0.61: 1.56, 0.62: 1.56, 0.63: 1.56, 0.64: 1.565,
+                    0.65: 1.565, 0.66: 1.565, 0.67: 1.565, 0.68: 1.57, 0.69: 1.57,
+                    0.70: 1.57, 0.71: 1.57, 0.72: 1.575, 0.73: 1.575, 0.74: 1.58,
+                    0.75: 1.58, 0.76: 1.58, 0.77: 1.59, 0.78: 1.59, 0.79: 1.60,
+                    0.80: 1.60, 0.81: 1.60, 0.82: 1.61, 0.83: 1.62, 0.84: 1.62,
+                    0.85: 1.62, 0.86: 1.65, 0.87: 1.70
+                    }
+
+# The servo hat uses its own numbering scheme within the Adafruit library.
+# 0 represents the first servo, 1 for the second, and so on.
+LSERVO = 0
+RSERVO = 1
+
+# Initialize the servo hat library.
+pwm = Adafruit_PCA9685.PCA9685()
+
+# 50Hz is used for the frequency of the servos.
+pwm.set_pwm_freq(50)
+
+# Pins that the encoders are connected to
+LENCODER = 17
+RENCODER = 18
+
+#Values
+lTickCount = 0
+rTickCount = 0
+lSpeed = 0
+rSpeed = 0
+currentTime = 0
+lRevolutions = 0
+rRevolutions = 0
+startTime = time.time()
+
+# Pins that the sensors are connected to
+LSHDN = 27
+FSHDN = 22
+RSHDN = 23
+
+DEFAULTADDR = 0x29 # All sensors use this address by default, don't change this
+LADDR = 0x2a
+RADDR = 0x2b
+
+# Set the pin numbering scheme to the numbering shown on the robot itself.
+GPIO.setmode(GPIO.BCM)
+
+# Setup pins
+GPIO.setup(LSHDN, GPIO.OUT)
+GPIO.setup(FSHDN, GPIO.OUT)
+GPIO.setup(RSHDN, GPIO.OUT)
+
+# Shutdown all sensors
+GPIO.output(LSHDN, GPIO.LOW)
+GPIO.output(FSHDN, GPIO.LOW)
+GPIO.output(RSHDN, GPIO.LOW)
+
+time.sleep(0.01)
+
+# Initialize all sensors
+lSensor = VL53L0X.VL53L0X(address=LADDR)
+fSensor = VL53L0X.VL53L0X(address=DEFAULTADDR)
+rSensor = VL53L0X.VL53L0X(address=RADDR)
+
+# Connect the left sensor and start measurement
+GPIO.output(LSHDN, GPIO.HIGH)
+time.sleep(0.01)
+lSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
+
+# Connect the right sensor and start measurement
+GPIO.output(RSHDN, GPIO.HIGH)
+time.sleep(0.01)
+rSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
+
+# Connect the front sensor and start measurement
+GPIO.output(FSHDN, GPIO.HIGH)
+time.sleep(0.01)
+fSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
+
+def ctrlC(signum, frame):
+    print("Exiting")
+    GPIO.cleanup()
+    ## Write an initial value of 1.5, which keeps the servos stopped.
+    ## Due to how servos work, and the design of the Adafruit library,
+    ## the value must be divided by 20 and multiplied by 4096.
+    pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
+    pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+    # Stop measurement for all sensors
+    lSensor.stop_ranging()
+    fSensor.stop_ranging()
+    rSensor.stop_ranging()
+    exit()
+
+# Function that flips pwm values since servos are in opposite directions
+def servoFlip(speed):
+	difference = speed - 1.5
+	return 1.5 - difference
+
+# Function that translates speeds from ips to pwm
+def setSpeedsIPS(ipsLeft, ipsRight):
+    # Converting inches per second into revolutions per second
+    rpsLeft = float(math.ceil((ipsLeft / 8.20) * 100) / 100)
+    rpsRight = float(math.ceil((ipsRight / 8.20) * 100) / 100)
+
+    # Flipping RPS values when negative in order to use the appropiate pwm values
+    if rpsLeft < 0:
+        rpsLeft = 0 - rpsLeft
+    if rpsRight < 0:
+        rpsRight = 0 - rpsRight
+
+    # Calculating pwm values from the respective dictionaries
+    lPwmValue = float(lPwmTranslation[rpsLeft])
+    rPwmValue = float(rPwmTranslation[rpsRight])
+
+    if ipsLeft < 0 and ipsRight < 0:
+        # Setting appropiate speeds to the servos when going forwards
+        pwm.set_pwm(LSERVO, 0, math.floor(lPwmValue / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(servoFlip(rPwmValue) / 20 * 4096))
+    elif ipsLeft >= 0 and ipsRight >= 0:
+        # Setting apporpiate speeds to the servos when going backwards
+        pwm.set_pwm(LSERVO, 0, math.floor(servoFlip(lPwmValue) / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(rPwmValue / 20 * 4096))
+    elif ipsLeft >= 0 and ipsRight < 0:
+		# Setting apporpiate speeds to the servos when turning
+        pwm.set_pwm(LSERVO, 0, math.floor(servoFlip(lPwmValue) / 20 * 4096))
+        pwm.set_pwm(RSERVO, 0, math.floor(servoFlip(rPwmValue) / 20 * 4096))
+
+# Function to set appropiate boundaries for front sensor
+def saturationFunction(ips):
+    controlSignal = ips
+
+    # Value of 0.5 is used to limit the range of speeds possible and was reached through trial and error
+    if controlSignal > 0.5:
+        controlSignal = 0.5
+    elif controlSignal < -0.5:
+        controlSignal = -0.5
+    return controlSignal
+
+# Function to set appropiate boundaries for right sensor
+def saturationFunctionRight(inches):
+    controlSignal = inches
+
+    #Value of 0.5 is used to limit the range of speeds possible and was reached through trial and error
+    if controlSignal > 0.5:
+        controlSignal = 0.5
+    elif controlSignal < -0.5:
+        controlSignal = -0.5
+    return controlSignal
+
+# Function to make a left turn when needed
+def turnLeft():
+    setSpeedsIPS(1.3, -2)
+    time.sleep(2.35)
+    setSpeedsIPS(0,0)
+    time.sleep(0.1)
+
+
+def setSpeedsvw(v, w):
+    leftSpeed1 = (v + (w*3.95))
+    rightSpeed1 = (v - (w*3.95))
+    setSpeedsIPS(-leftSpeed1, -rightSpeed1)
+
+# Declaring the disared distance to the wall
+desiredDistance = 5.0
+
+# Declaring the kp value to be used
+kpValue = 0.9
+
+# Sleeping the motors before starting the movement
+pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
+pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
+time.sleep(3)
+
+# Waiting for user to enter the required key in order to start the movement
+flagStart = False
+startInput = input("Press 'm' to start robot wall following.")
+if startInput == "m":
+	flagStart = True
+else:
+	print("Exiting program, re-run file wall following.")
+	exit()
+
+# Declaring constant linear speed that will be used during the movement
+linearSpeed = 5
+
+# Declaring a variable to keep track of front sensor big readings
+sensorCount = 0
+
+while True:
+    # Reading in from sensors
+    fDistance = fSensor.get_distance()
+    rDistance = rSensor.get_distance()
+
+    # Transforming readings to inches
+    inchesDistanceFront = fDistance * 0.0393700787
+    inchesDistanceRight = rDistance * 0.0393700787
+
+    # Calculating respective errors
+    errorf = 5.0 - inchesDistanceFront
+    errorr = 5.0 - inchesDistanceRight
+
+    # Computing the control signals
+    controlSignalf = kpValue * errorf
+    controlSignalr = kpValue * errorr
+
+    # Running control signals through saturation functions
+    newSignalf = saturationFunction(controlSignalf)
+    newSignalr = saturationFunctionRight(controlSignalr)
+
+    # Setting speed of the robot, angular speed will be zero when moving straight
+    setSpeedsvw(linearSpeed,-newSignalr)
+
+    # Checking if there is an object approaching from the front
+    if inchesDistanceFront < 5.0:
+        # Increasing reading count
+	    sensorCount += 1
+
+        # Checking if the front small reading happens continously to avoid a fake trigger
+	    if sensorCount > 4:
+            # Turning left
+		    turnLeft()
+
+    # Clearing sensor count for continous small front readings
+    else:
+        sensorCount = 0
