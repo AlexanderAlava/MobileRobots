34d2798ca4a8a5a6fd19b3cf991c2b694bfa2006
AlexanderAlava <alava@mail.usf.edu>
2018-11-26 00:48:25 -0500

    Change time to 2.0 secs

--- Lab4/FinishedCode/Navigation.py
+++ Lab4/FinishedCode/Navigation.py
@@ -497,7 +497,7 @@ def inPlaceTurnAround():
     stop()
     pwm.set_pwm(LSERVO, 0, math.floor(1.55 / 20 * 4096))
     pwm.set_pwm(RSERVO, 0, math.floor(1.55 / 20 * 4096))
-    time.sleep(1.9)
+    time.sleep(2.0)
     lRevolutions = 1.1
     rRevolutions = 1.1
 
